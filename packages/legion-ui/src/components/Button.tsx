52420e2d144818af2e5eb18a9577f2b3741f258f
fahrulseptiana <10472373+fahrulseptiana@users.noreply.github.com>
2022-01-22 12:24:49 +0700

    feat: add more variant support

--- packages/legion-ui/src/components/Button.tsx
+++ packages/legion-ui/src/components/Button.tsx
@@ -3,10 +3,12 @@
 import {jsx} from '@theme-ui/core'
 import {Button as Component, ButtonProps as Props} from '@theme-ui/components'
 
-export interface ButtonProps extends Props {}
+export interface ButtonProps extends Props {
+  variant: 'solid' |'outline' | 'soft' | 'transparent' | 'outlineSecondary' | 'softSecondary'
+}
 
-export const Button:React.FC<ButtonProps> = ({children, ...rest}:ButtonProps) => (
-  <Component {...rest}>{children}</Component>
+export const Button:React.FC<ButtonProps> = ({variant='solid', children, ...rest}:ButtonProps) => (
+  <Component variant={variant} {...rest}>{children}</Component>
 )
 
 export default Button
